c7c5fef56f65bada86868ac35371d4e4497fd4a7
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-11-06 21:23:20 +0900

    fix

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mattn/go-hsd
+
+go 1.18
---
+++ hsd_test.go
@@ -0,0 +1,29 @@
+package hsd
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestStringDistance(t *testing.T) {
+	tests := []struct {
+		name string
+		lhs  string
+		rhs  string
+		want int
+	}{
+		{name: "lhs is longer than rhs", lhs: "foo", rhs: "fo", want: -1},
+		{name: "rhs is shorter than rhs", lhs: "fo", rhs: "foo", want: -1},
+		{name: "1 diff", lhs: "foo", rhs: "foh", want: 1},
+		{name: "2 diffs", lhs: "foo", rhs: "fhh", want: 2},
+		{name: "3 diffs", lhs: "foo", rhs: "hhh", want: 3},
+		{name: "multibyte", lhs: "あいう", rhs: "あいえ", want: 1},
+	}
+
+	for _, tc := range tests {
+		got := StirngDistance(tc.lhs, tc.rhs)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
+		}
+	}
+}
